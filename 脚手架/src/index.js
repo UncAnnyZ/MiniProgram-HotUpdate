b3797b1f5803739bf71cf174b8a88fef07ef3c0b
UncAnnyZ <49876144+UncAnnyZ@users.noreply.github.com>
2022-01-10 13:04:16 +0800

    wxml更新canvas
    wxml更新canvas

--- 脚手架/src/index.js
+++ 脚手架/src/index.js
@@ -1,57 +1,69 @@
-// import parseTag from '../ast.js'
+var chart = require("./utils/chart");
 
-
+console.log(chart)
 
 function runCode(that, e) {
 
-    wx.setNavigationBarTitle({ title: 'We校园-倒数日' });
-  
-    wx.setNavigationBarColor({
-      backgroundColor: '#eeeeee',
-      frontColor: '#000000',
-    })
-    const a = () => {
-      
-    }
-  
+    wx.setNavigationBarTitle({ title: '热更新' });
+
+
     that.data = {
-      index : 0,
-      gapDays: "1",
-      Name: "hello world",
-      Date: "2022.01.01"
+        index: 0,
+    }
+    
+    that.onload = function() {
+        chart.draw(that, 'canvas1', {
+            title: {
+              text: "2017城市人均收入(万)",
+              color: "#333333"
+            },
+            xAxis: {
+              data: ['北京', '上海', '杭州', '深圳', '广州', '成都', '南京', '西安']
+            },
+            series: [
+              // {
+              //   name: "第一季度",
+              //   category: "bar",
+              //   data: [37, 63, 60, 78, 92, 63, 57, 48]
+              // },
+              // {
+              //   name: "第二季度",
+              //   category: "line",
+              //   data: [20, 35, 38, 59, 48, 27, 43, 35]
+              // },
+              {
+                name: ['北京', '上海', '杭州', '深圳', '广州', '成都'],
+                category: "pie",
+                data: [40, 38, 39, 28, 27, 33]
+              }
+            ]
+          });
+          that.reSetPage()
+    }
+
+    that.onSaveClick = function () {
+        chart.saveCanvans(function () {
+    
+        });
     }
     //每一次刷新建议重新调用
     that.reSetPage = function () {
-      that.data.html =
-        `
-        <view class="page">
-        <view class="all">
-        <view class="flex-item"  data-index="${that.data.index}">  
-            <view class="flex-item_title">
-            ${that.data.gapDays > 0 ? ` <text >${that.data.Name} 还有</text>` : `    <text>${that.data.Name} 已经</text>`}
-  
+        that.data.html =
+            `
+      
         
-            </view>
-            <view class="flex-item_gapDays">
-            ${that.data.gapDays > 0 ? ` <text >${that.data.gapDays}</text>` : `  <text >${0-that.data.gapDays}</text>`}
-            </view>
-            <view class="flex-item_desc">
-            <text>目标日：${that.data.Date}</text>
-            </view>
-        </view>
-        </view>
-        </view>
+        <canvas canvas-id="canvas1"  style="background-color: #f4f4f4;width:${that.data.chartWidth}px;height:${that.data.chartHeight}px;"/>
+        <button class="downloadChart" bindtap="onSaveClick">保存图片</button>
       `
-      that.setData({
-        html: that.parse(that.data.html)
-      });
+        that.setData({
+            html: that.parse(that.data.html)
+        });
     };
-  
+
     that.reSetPage()
-  
-    // that.onload()
-  
-  }
-  
-  module.exports = runCode;
-  
+
+    that.onload()
+
+}
+
+window.exports = runCode;
